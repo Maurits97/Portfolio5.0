db2d4affd31dd436451013126c90ef1ffddd6530
Maurits Brouwer <>
2019-05-19 11:51:02 +0200

    feat (added arrow)

--- js/script-animation.js
+++ js/script-animation.js
@@ -1,6 +1,6 @@
 var header = $('.header');
 var headerNumber = $('.header__number');
-var headlineNumber = $('.headline__number');
+var arrow = $('.arrow');
 
 
 $(window).on('scroll', function() {
@@ -8,6 +8,7 @@ $(window).on('scroll', function() {
    if(st < 180){
    	header.css({ 'opacity' : (1 - st/200) });
    	headerNumber.css({ 'margin-left' : ((120 - (st * 0.555)) + 'px')});
+   	arrow.css({ 'opacity' : (1 - st/90)});
    } else {
    	header.css({ 'opacity' : 0.1 });
    	headerNumber.css({ 'margin-left' : (20 + 'px')});
