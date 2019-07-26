0420aff2de074a09e1ecca42dce10a35810a095d
Maurits Brouwer <>
2019-07-26 13:19:50 +0200

    feat (grid bug fix)

--- js/script-animation.js
+++ js/script-animation.js
@@ -1,15 +1,16 @@
-var header = $('.header');
 var headerNumber = $('.header__number');
+var header = $('.header');
 var arrow = $('.arrow');
+console.log($(document).width());
 
 $(window).on('scroll', function() {
    var st = $(this).scrollTop();
    if(st < 180){
+   		headerNumber.css({ 'margin-left' : ((120 - (st * 0.555)) + 'px')});
    	header.css({ 'opacity' : (1 - st/200) });
-   	headerNumber.css({ 'margin-left' : ((120 - (st * 0.555)) + 'px')});
    	arrow.css({ 'opacity' : (1 - st/90)});
    } else {
+   		headerNumber.css({ 'margin-left' : (20 + 'px')});
    	header.css({ 'opacity' : 0.1 });
-   	headerNumber.css({ 'margin-left' : (20 + 'px')});
    }
 });
