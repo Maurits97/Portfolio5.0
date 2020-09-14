33416bbc50c97053d407b9da971d4d033d793c6f
Maurits Brouwer <>
2020-09-14 13:23:42 +0200

    feat (new work)

--- js/script-vue.js
+++ js/script-vue.js
@@ -75,7 +75,7 @@ var aboutme = new Vue({
 		firstname: 'Maurits',
 		surname: 'Brouwer',
 		intro: 'Hi, ik ben Maurits Brouwer, een enthousiaste front end developer met een passie voor web design. Ik maak coole digitale producten en zorg dat de gebruiker altijd de juiste experience heeft.',
-		study: 'Op dit moment zit ik in mijn 4de jaar van de studie Communication & Multimedia Design aan de Haagse Hogeschool.',
+		study: 'Ik ben afgestudeerd bij Communication & Multimedia Design aan de Haagse Hogeschool.',
 		image: {
 			portret: 'img/Portfolio_Website_Maurits_Brouwer.jpg',
 			signature: 'img/Maurits_Brouwer_Signature_Web.png',
@@ -98,59 +98,45 @@ var aboutme = new Vue({
 var work = new Vue({
 	el: '.work',
 	data: {
-		item_1: {
-			image: 'img/Maurits_Brouwer_Item_Portfolio_Website1.png',
-			link: 'https://oud.mauritsbrouwer.nl',
-			header: 'Portfolio Website 1.0',
-			text: 'Mijn allereerste programmeer project was mijn eerste portfolio website. Deze heb ik gemaakt voor een projectvak tijdens mijn eerste jaar van mijn studie. Hoewel er veel techniek ontbrak was ik er erg trots op. Zo is de website niet responsive en vind ik hem inhoudelijk niet erg sterk. Wel was ik trots op het gebruik van het parallax effect. Kortom: een leuke ervaring om op terug te kijken.',
-		},
+		work: {
+			item_1: {
+				image: 'img/Maurits_Brouwer_Item_BrigitteStarkPortfolio.png',
+				link: '',
+				header: 'Art portfolio',
+				text: 'Het art portfolio is in opdracht van Brigitte Stark gemaakt. Bij deze opdracht was ik verantwoordelijk voor zowel het design als de development. De website is op een efficiente manier in elkaar gezet dankzij het Vue.js Framework. Met behulp van vue.js bleef mijn code "DRY" en kost het nauwelijks tijd om een nieuw kunstwerk aan de website toe te voegen.'
+			},
+			item_2: {
+				image: 'img/Maurits_Brouwer_Item_Denkeffekt.jpg',
+				link: 'https://denkeffekt.nl',
+				header: 'Denkeffekt Remedial Teaching',
+				text: 'Denkeffekt is een praktijk voor bijles en coaching. Voor denkeffekt was ik verantwoordelijk voor zowel het design als de development. De website is zonder frameworks gemaakt. '
+			},
+			item_3: {
+				image: 'img/Maurits_Brouwer_Item_Portfolio_Website1.png',
+				link: 'https://oud.mauritsbrouwer.nl',
+				header: 'Portfolio Website 1.0',
+				text: 'Mijn allereerste programmeer project was mijn eerste portfolio website. Deze heb ik gemaakt voor een projectvak tijdens mijn eerste jaar van mijn studie. Hoewel er veel techniek ontbrak was ik er erg trots op. Zo is de website niet responsive en vind ik hem inhoudelijk niet erg sterk. Wel was ik trots op het gebruik van het parallax effect. Kortom: een leuke ervaring om op terug te kijken.',
+			},
 
-		item_2: {
-			image: 'img/Maurits_Brouwer_Item_HCI.jpeg',
-			link: 'https://hci.mauritsbrouwer.nl',
-			header: 'HCI Technologies',
-			text: 'Mijn 2de jaar eindigde met het vak: HCI Technologies. Het leukste bij dit vak waren de Lab Weeks. Je kon hiervoor een thema kiezen en aan de hand daarvan kwam je in een groep terecht. Ik had gekozen voor het thema AR & VR. Samen met mijn groep hadden wij een VR escape room gemaakt. In deze virtuele wereld zat je vast in een kamer en moest je doormiddel van puzzels de deur openen. Deze puzzels waren echter onmogelijk om alleen oplossen. Gelukkig was er een tweede speler die doormiddel van Augmented Reality hints kon vinden. De catch hierin was, dat de speler in de virtuele wereld niet terug kon praten. Wat ons product uniek maakte was dat AR en VR met elkaar gecombineerd werd. Ons concept was eigenlijk voortgekomen uit een spelletje genaamd “Keep Talking and Nobody Explodes”. Dit is een bom spelletje waar één iemand een bom heeft, en de andere persoon de “manual”. Hierin staat hoe je de bom onschadelijk kan maken. Het idee om elkaar te helpen terwijl je niet in dezelfde kamer bent vonden wij een goed begin om verder mee te gaan.',
-		},
-
-		item_3: {
-			image: 'img/Maurits_Brouwer_Item_Dept.jpg',
-			link: 'https://maurits97.github.io/DeptOpdracht/assets/index.html',
-			header: 'Fictieve opdracht: Dept Agency',
-			text: 'Tijdens het zoeken van een stageplek, heb ik het ook bij Dept Agency geprobeerd. Ook hierbij kreeg ik een fictieve opdracht zodat zij inzicht konden krijgen wat mijn huidige skills waren. Van deze opdracht heb ik erg veel van geleerd. Ik maakte hierbij kennis met technieken zoals Gulp en Sass.',
+			item_6: {
+				image: 'img/Maurits_Brouwer_Item_HCI.jpeg',
+				link: 'https://hci.mauritsbrouwer.nl',
+				header: 'HCI Technologies',
+				text: 'Mijn 2de jaar eindigde met het vak: HCI Technologies. Het leukste bij dit vak waren de Lab Weeks. Je kon hiervoor een thema kiezen en aan de hand daarvan kwam je in een groep terecht. Ik had gekozen voor het thema AR & VR. Samen met mijn groep hadden wij een VR escape room gemaakt. In deze virtuele wereld zat je vast in een kamer en moest je doormiddel van puzzels de deur openen. Deze puzzels waren echter onmogelijk om alleen oplossen. Gelukkig was er een tweede speler die doormiddel van Augmented Reality hints kon vinden. De catch hierin was, dat de speler in de virtuele wereld niet terug kon praten. Wat ons product uniek maakte was dat AR en VR met elkaar gecombineerd werd. Ons concept was eigenlijk voortgekomen uit een spelletje genaamd “Keep Talking and Nobody Explodes”. Dit is een bom spelletje waar één iemand een bom heeft, en de andere persoon de “manual”. Hierin staat hoe je de bom onschadelijk kan maken. Het idee om elkaar te helpen terwijl je niet in dezelfde kamer bent vonden wij een goed begin om verder mee te gaan.',
+			},
 		},
 	},
 	template: `
 		<div class='work'>
-			<div class="work__item">
-				<a :href='item_1.link' target='blank'><img :src='item_1.image' class="work__item__image" /></a>
-				<h3 class='work__item__header'> {{ item_1.header}} </h3>
+			<div class="work__item" v-for="item in work">
+				<a :href='item.link' target='blank'><img :src='item.image' class="work__item__image" /></a>
+				<h3 class='work__item__header'> {{item.header}} </h3>
 				<div class='work__item__link'>
-					<a :href='item_1.link' target='blank' ><i class="arrow__circle material-icons-outlined">arrow_drop_down_circle</i>Ga naar..</a>
+					<a :href='item.link' target='blank' ><i class="arrow__circle material-icons-outlined">arrow_drop_down_circle</i>Ga naar..</a>
 				</div>
-				<p class='work__item__text'> {{ item_1.text }}</p>
+				<p class='work__item__text'> {{ item.text }}</p>
 				<div class='work__item__underline'></div>
 			</div>
-
-			<div class="work__item">
-				<a :href='item_2.link' target='blank'><img :src='item_2.image' class="work__item__image" /></a>
-				<h3 class='work__item__header'> {{ item_2.header}} </h3>
-				<div class='work__item__link'>
-					<a :href='item_2.link' target='blank'><i class="arrow__circle material-icons-outlined">arrow_drop_down_circle</i>Ga naar..</a>
-				</div>
-				<p class='work__item__text'> {{ item_2.text }}</p>
-				<div class='work__item__underline'></div>
-			</div>
-
-			<div class="work__item">
-				<a :href='item_3.link' target='blank'><img :src='item_3.image' class="work__item__image" /></a>
-				<h3 class='work__item__header'> {{ item_3.header}} </h3>
-				<div class='work__item__link'>
-					<a :href='item_3.link' target='blank' ><i class="arrow__circle material-icons-outlined">arrow_drop_down_circle</i>Ga naar..</a>
-				</div>
-				<p class='work__item__text'> {{ item_3.text }}</p>
-				<div class='work__item__underline'></div>
-			</div>
-
 		</div>
 	`
 })
@@ -161,7 +147,6 @@ var footer = new Vue({
 		header: 'Zo kan je mij contacteren.',
 		credentials: {
 			email: 'MauritsBrouwer@outlook.com' ,
-			phone: '+ 06 57276646',
 			other: {
 				title: 'Of neem een kijkje op mijn…',
 				linkedin: 'Linkedin',
@@ -177,9 +162,6 @@ var footer = new Vue({
 			</h1>
 			<p class='contact__credentials'>
 				{{ credentials.email }}
-			</p>
-			<p class='contact__credentials'>
-				{{ credentials.phone }}
 			</p>
 
 			<div class='contact__other'>
