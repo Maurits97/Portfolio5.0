d738191226652f3b816a4a3c3c96ddc6bf7ccc9c
Maurits Brouwer <>
2020-09-25 14:45:38 +0200

    Update script-animation.js

--- js/script-animation.js
+++ js/script-animation.js
@@ -5,17 +5,21 @@ var title = $('.title');
 
 $(window).on('scroll', function() {
 	var st = $(this).scrollTop();
-  if(st < 180){
-  	if (screen.width > 768){
-		headerNumber.css({ 'margin-left' : ((120 - (st * 0.555)) + 'px')});
-   		title.css({ 'top' : ((60 - (st * 0.222)) + 'px')});
-   	}
+	console.log(screen.width );
+	if(st < 180){
+		if (screen.width > 768){
+			headerNumber.css({ 'margin-left' : ((120 - (st * 0.555)) + 'px')});
+   			title.css({ 'top' : ((60 - (st * 0.222)) + 'px')});
+		}
+		   
    	header.css({ 'opacity' : (1 - st/200) });
-   	arrow.css({ 'opacity' : (1 - st/90)});
+	arrow.css({ 'opacity' : (1 - st/90)});
+	   
    } else {
-   	if (screen.width > 768){
-   		headerNumber.css({ 'margin-left' : (20 + 'px')});	
-   	} 
+	   if (screen.width > 768){
+		   headerNumber.css({ 'margin-left' : (20 + 'px')});	
+		}
+		    
    	header.css({ 'opacity' : 0.1 });
    }
 });
